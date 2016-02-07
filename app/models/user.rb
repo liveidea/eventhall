5d283b5031c5dd593569aaf7136c5f9de720f2c0
= <=>
2016-02-07 15:07:13 +0200

    added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ActiveRecord::Base
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
    has_many :halls
-   before_save { self.email = email.downcase }
+   # before_save { self.email = email.downcase }
 
    validates :name, presence: true, length: {in: 2..30}
-   validates_format_of :name, :with => /^[-a-z]+$/
+   # validates_format_of :name, :with => /^[-a-z]+$/
 
    validates :last_name, presence: true, length: {in: 2..40}
-   validates_format_of :last_name, :with => /^[-a-z]+$/
+   # validates_format_of :last_name, :with => /^[-a-z]+$/
 
-   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: { case_sensitive: false }
+   # validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: { case_sensitive: false }
 
    validates :phone, numericality: true, length: {maximum: 20}
 
--- db/migrate/20160207092417_create_users.rb
+++ db/migrate/20160207092417_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :last_name
-      t.string :email
+      # t.string :email
       t.string :phone
 
       t.timestamps null: false
