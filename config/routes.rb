da901dfdacfe45b4bdfa435842b92efac538f698
= <vesna95@ukr.net>
2016-02-13 12:25:12 +0200

    fixed sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-# root to: "home#index"
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+ # root  "home#index"
 
 
 end
