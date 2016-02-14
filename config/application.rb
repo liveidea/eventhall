2695da122269ab29500142215360106c78ee2ba3
grek-andrian <gretsko.andrian@gmail.com>
2016-02-14 12:49:07 +0200

    modified config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,3 +25,13 @@ module Eventhall
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+config.generators do |generate|
+  generate.test_framework  :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+   generate.fixture_replacement :factory_girl, dir: "spec/factories"
+end
