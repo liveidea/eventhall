563ac324024db3857b81a50a44a6907de9c82ef8
= <vesna95@ukr.net>
2016-02-21 15:53:57 +0200

    deleted links from nav

--- app/models/venue_type.rb
+++ app/models/venue_type.rb
@@ -1,8 +1,4 @@
 class VenueType < ActiveRecord::Base
   has_and_belongs_to_many :halls
 
-   def self.search search_term
-  return scoped unless search_term.present?
-  where(['name LIKE ?', "%#{search_term}%"])
-end
 end
