97370dee03a4d6360088c9b95ef9bb8d12c8fb62
= <vesna95@ukr.net>
2016-02-23 19:34:25 +0200

    added Image preview in Edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,17 @@
 
 
 $('.selectpicker').selectpicker();
+
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
+
+  reader.addEventListener("load", function () {
+    preview.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( variables.css )
 Rails.application.config.assets.precompile += %w( bootstrap-select.css )
 Rails.application.config.assets.precompile += %w( bootstrap-select.js )
+
