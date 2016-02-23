4099eba5a741f57e500e9e0ceb8815effbbfbee6
= <vesna95@ukr.net>
2016-02-23 16:21:03 +0200

    styled the Edit page

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -9,7 +9,7 @@ class Hall < ActiveRecord::Base
   has_and_belongs_to_many :event_types
   has_and_belongs_to_many :venue_types
 
-  validates :description, presence: true, length: {in: 5..255}
+  validates :description, presence: true, length: {in: 5..500}
   validates :price, presence: true, numericality: true, length: {maximum: 4}
   validates :name, presence: true, length: {maximum: 30}
   validates_presence_of :photos
