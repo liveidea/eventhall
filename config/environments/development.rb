ac47807ccd06ba81dec587d0ceb086ef721a75f8
= <vesna95@ukr.net>
2016-03-13 16:51:48 +0200

    added placeholder if no avatar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-I18n.available_locales = [:en, :uk]
-config.i18n.default_locale = :uk
+I18n.available_locales = [:en, :uk, :de]
+config.i18n.default_locale = :de
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-I18n.available_locales = [:en, :uk]
-config.i18n.default_locale = :uk
+  I18n.available_locales = [:en, :uk, :de]
+  config.i18n.default_locale = :de
   # Code is not reloaded between requests.
   config.cache_classes = true
 
