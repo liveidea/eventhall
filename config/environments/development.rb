30766af3777099ab8da248f1e722463dc264e6d1
= <vesna95@ukr.net>
2016-03-13 16:55:34 +0200

    fixed translation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-I18n.available_locales = [:en, :uk, :de]
-config.i18n.default_locale = :de
+I18n.available_locales = [:en, :uk]
+config.i18n.default_locale = :uk
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
