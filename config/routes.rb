efec6c0eeaeeb8e9cf01f19f4be7733f2601d6a5
= <vesna95@ukr.net>
2016-08-03 14:56:04 +0300

    Fixed ERR_TOO_MANY_REDIRECTS

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,5 @@ scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
   get 'our_team' => 'halls#team'
 end
 
-get '*path', to: redirect("/#{I18n.default_locale}/%{path}")
-get '', to: redirect("/#{I18n.default_locale}")
 
 end
